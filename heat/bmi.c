5ec9f7f09f57eeb2b517023d82474a0b78a7d5de
mcflugen <mcflugen@gmail.com>
2015-02-26 22:45:53 -0700

    Use call_bmi_and_return macro for BMI calls.

--- heat/bmi.c
+++ heat/bmi.c
@@ -1,5 +1,15 @@
 #include "bmi.h"
-#include <stdio.h>
+
+
+#define initialized_or_return(model) { \
+  if (!model || !model->self) return BMI_FAILURE; \
+}
+
+#define call_bmi_and_return(model, func, ...) { \
+  initialized_or_return(model); \
+  return model->func (model->self, ##__VA_ARGS__); \
+}
+
 
 int
 BMI_Initialize(BMI_Model *model, const char * config_file)
@@ -8,194 +18,114 @@ BMI_Initialize(BMI_Model *model, const char * config_file)
 }
 
 
-int
-BMI_Update(BMI_Model *model)
-{
-  if (model && model->self)
-    return model->Update(model->self);
-  else
-    return 1;
+int BMI_Update(BMI_Model *model) {
+  call_bmi_and_return(model, Update);
 }
 
 
-int
-BMI_Update_until(BMI_Model *model, double time)
-{
-  return model->Update_until(model->self, time);
+int BMI_Update_until(BMI_Model *model, double time) {
+  call_bmi_and_return(model, Update_until, time);
 }
 
 
-int
-BMI_Update_frac(BMI_Model *model, double fraction)
-{
-  return model->Update_frac(model->self, fraction);
+int BMI_Update_frac(BMI_Model *model, double fraction) {
+  call_bmi_and_return(model, Update_frac, fraction);
 }
 
 
-int
-BMI_Finalize(BMI_Model *model)
-{
-  return model->Finalize(model->self);
+int BMI_Finalize(BMI_Model *model) {
+  call_bmi_and_return(model, Finalize);
 }
 
 
-int
-BMI_Run_model(BMI_Model *model)
-{
-  if (model && model->self)
-    return model->Run_model(model->self);
-  else
-    return 1;
+int BMI_Run_model(BMI_Model *model) {
+  call_bmi_and_return(model, Run_model);
 }
 
 
-int
-BMI_Get_component_name(BMI_Model* model, char * name)
-{
-  if (model && model->self)
-    return model->Get_component_name(model->self, name);
-  else
-    return 1;
-}
-
-int
-BMI_Get_input_var_name_count(BMI_Model * model, int * count)
-{
-  return model->Get_input_var_name_count(model->self, count);
+int BMI_Get_component_name(BMI_Model* model, char * name) {
+  call_bmi_and_return(model, Get_component_name, name);
 }
 
 
-int
-BMI_Get_output_var_name_count(BMI_Model * model, int * count)
-{
-  return model->Get_output_var_name_count(model->self, count);
+int BMI_Get_input_var_name_count(BMI_Model * model, int * count) {
+  call_bmi_and_return(model, Get_input_var_name_count, count);
 }
 
 
-int
-BMI_Get_input_var_names(BMI_Model * model, char ** names)
-{
-  return model->Get_input_var_names(model->self, names);
+int BMI_Get_output_var_name_count(BMI_Model * model, int * count) {
+  call_bmi_and_return(model, Get_output_var_name_count, count);
 }
 
 
-int
-BMI_Get_output_var_names(BMI_Model * model, char ** names)
-{
-  return model->Get_output_var_names(model->self, names);
+int BMI_Get_input_var_names(BMI_Model * model, char ** names) {
+  call_bmi_and_return(model, Get_input_var_names, names);
 }
 
 
-int
-BMI_Get_time_step(BMI_Model* model, double * dt)
-{
-  if (model && model->self)
-    return model->Get_time_step(model->self, dt);
-  else
-    return 1;
+int BMI_Get_output_var_names(BMI_Model * model, char ** names) {
+  call_bmi_and_return(model, Get_output_var_names, names);
 }
 
 
-int
-BMI_Get_current_time(BMI_Model* model, double * time)
-{
-  if (model && model->self)
-    return model->Get_current_time(model->self, time);
-  else
-    return 1;
+int BMI_Get_time_step(BMI_Model* model, double * dt) {
+  call_bmi_and_return(model, Get_time_step, dt);
 }
 
 
-int
-BMI_Get_var_type(BMI_Model * model, const char * name, char * type)
-{
-  if (model && model->self)
-    return model->Get_var_type(model->self, name, type);
-  else
-    return 1;
+int BMI_Get_current_time(BMI_Model* model, double * time) {
+  call_bmi_and_return(model, Get_current_time, time);
 }
 
 
-int
-BMI_Get_var_units(BMI_Model * model, const char * name, char * units)
-{
-  if (model && model->self)
-    return model->Get_var_units(model->self, name, units);
-  else
-    return 1;
+int BMI_Get_var_type(BMI_Model * model, const char * name, char * type) {
+  call_bmi_and_return(model, Get_var_type, name, type);
 }
 
 
-int
-BMI_Get_var_rank(BMI_Model * model, const char * name, int * rank)
-{
-  if (model && model->self)
-    return model->Get_var_rank(model->self, name, rank);
-  else
-    return 1;
+int BMI_Get_var_units(BMI_Model * model, const char * name, char * units) {
+  call_bmi_and_return(model, Get_var_units, name, units);
 }
 
 
-int
-BMI_Get_grid_shape(BMI_Model * model, const char * name, int * shape)
-{
-  if (model && model->self)
-    return model->Get_grid_shape(model->self, name, shape);
-  else
-    return 1;
+int BMI_Get_var_rank(BMI_Model * model, const char * name, int * rank) {
+  call_bmi_and_return(model, Get_var_rank, name, rank);
 }
 
 
-int
-BMI_Get_grid_spacing(BMI_Model * model, const char * name, double * spacing)
-{
-  if (model && model->self)
-    return model->Get_grid_spacing(model->self, name, spacing);
-  else
-    return 1;
+int BMI_Get_grid_shape(BMI_Model * model, const char * name, int * shape) {
+  call_bmi_and_return(model, Get_grid_shape, name, shape);
 }
 
 
-int
-BMI_Get_grid_origin(BMI_Model * model, const char * name, double * origin)
-{
-  if (model && model->self)
-    return model->Get_grid_origin(model->self, name, origin);
-  else
-    return 1;
+int BMI_Get_grid_spacing(BMI_Model * model, const char * name,
+    double * spacing) {
+  call_bmi_and_return(model, Get_grid_spacing, name, spacing);
 }
 
 
-int
-BMI_Get_value(BMI_Model * model, const char * name, void * buffer)
-{
-  if (model && model->self)
-    return model->Get_value(model->self, name, buffer);
-  else
-    return 1;
+int BMI_Get_grid_origin(BMI_Model * model, const char * name,
+    double * origin) {
+  call_bmi_and_return(model, Get_grid_origin, name, origin);
 }
 
 
-int
-BMI_Get_value_ptr(BMI_Model * model, const char * name, void **ptr)
-{
-  if (model && model->self)
-    return model->Get_value_ptr(model->self, name, ptr);
-  else
-    return 1;
+int BMI_Get_value(BMI_Model * model, const char * name, void * buffer) {
+  call_bmi_and_return(model, Get_value, name, buffer);
 }
 
 
-int
-BMI_Set_value(BMI_Model * model, const char * name, void * buffer)
-{
-  return model->Set_value(model->self, name, buffer);
+int BMI_Get_value_ptr(BMI_Model * model, const char * name, void **ptr) {
+  call_bmi_and_return(model, Get_value_ptr, name, ptr);
 }
 
 
-int
-BMI_Set_value_at_indices(BMI_Model * model, const char * name, int * indices,
-    int len, void * buffer)
-{
-  return model->Set_value_at_indices(model->self, name, indices, len, buffer);
+int BMI_Set_value(BMI_Model * model, const char * name, void * buffer) {
+  call_bmi_and_return(model, Set_value, name, buffer);
+}
+
+
+int BMI_Set_value_at_indices(BMI_Model * model, const char * name,
+    int * indices, int len, void * buffer) {
+  call_bmi_and_return(model, Set_value_at_indices, name, indices, len, buffer);
 }
