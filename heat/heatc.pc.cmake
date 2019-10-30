02c22202bd5b9d5fd8b7f2b456a857bdb2d1d62c
Mark Piper <mark.piper@colorado.edu>
2019-10-30 16:06:54 -0600

    Update name and library name
    Changing to HeatC so we can also use HeatCXX. The library name follows
    the pattern set in the Fortran example, resulting in `libbmiheatc.so` (on
    Linux, yeah).

--- heat/heatc.pc.cmake
+++ heat/heatc.pc.cmake
@@ -1,5 +1,5 @@
-Name: Heat
+Name: HeatC
 Description: 2D Heat Equation
 Version: ${HEAT_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_heatc
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmiheatc
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include
